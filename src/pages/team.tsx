589c9ca1302d93115be7e3726159e36cf85e316f
yannisleng <lengzhiyan1015@gmail.com>
2023-12-07 18:10:08 +0800

    Request: Added blog page UI

---
+++ src/pages/team.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Profile from "~/components/Profile";
+import SectionHeader from "~/components/SectionHeader";
+
+// TODO
+// 1. Add link to GitHub etc.
+// 2. Set responsive view
+
+export default function team() {
+  const members = [
+    {
+      name: "Innis",
+      position: "Full Stack Engineer",
+      profilePicPath: "t1",
+    },
+    {
+      name: "Yannis",
+      position: "Full Stack Engineer",
+      profilePicPath: "t2",
+    },
+    {
+      name: "Jia Hao",
+      position: "Full Stack Engineer",
+      profilePicPath: "t3",
+    },
+    {
+      name: "Innis",
+      position: "Full Stack Engineer",
+      profilePicPath: "t1",
+    },
+    {
+      name: "Yannis",
+      position: "Full Stack Engineer",
+      profilePicPath: "t2",
+    },
+    {
+      name: "Jia Hao",
+      position: "Full Stack Engineer",
+      profilePicPath: "t3",
+    },
+  ];
+  return (
+    <>
+      <SectionHeader
+        title="Team"
+        desc="Our Professional Team, committed to providing the best service."
+      ></SectionHeader>
+
+      <div className="mt-9 flex flex-col items-center">
+        <div className="grid w-[90%] grid-cols-4 gap-x-2 gap-y-12">
+          {members.map((profile, index) => {
+            return (
+              <Profile
+                key={index}
+                name={profile.name}
+                position={profile.position}
+                profilePicPath={profile.profilePicPath}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
