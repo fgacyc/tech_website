c77e79e737f98f8eb46b2f75a613a41bc6ebca74
whyiwant <youyuanchong@gmail.com>
2023-12-11 18:15:56 +0800

    Request: use semantic elements like h1-h6 for text and button

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button, Card, CardBody, CardHeader, Image } from "@nextui-org/react";
 
 interface SectionHeaderProps {
   title: string;
@@ -11,8 +10,12 @@ const SectionHeader = (props: SectionHeaderProps) => {
     <>
       <div className="flex flex-col items-center bg-[#1d2129] pt-12 text-white">
         <div className="w-[90%]">
-          <div className="flex  text-4xl font-black">{props.title}</div>
-          <div className="font-xl mt-4 flex">{props.desc}</div>
+          <div className="flex font-black">
+            <h1>{props.title}</h1>
+          </div>
+          <div className="mt-4 flex">
+            <h3>{props.desc}</h3>
+          </div>
         </div>
       </div>
     </>
--- src/components/team/JoinCard.tsx
+++ src/components/team/JoinCard.tsx
@@ -4,15 +4,17 @@ import { IoIosArrowForward } from "react-icons/io";
 const JoinCard = () => {
   return (
     <div className="mt-12 flex w-full flex-col items-center pb-12">
-      <div className="w-[90%] bg-gradient-to-r from-slate-900 to-slate-50 pl-12 pt-7 text-white">
-        <div className="flex text-4xl font-black">FGA Tech Team</div>
+      <div className="w-[90%] rounded-xl bg-gradient-to-r from-slate-900 to-slate-50 pl-12 pt-7 text-white">
+        <div className="flex font-black">
+          <h2>FGA Tech Team</h2>
+        </div>
         <div className="font-xl mb-6 mt-4 flex">
-          We are waiting you, join us now!
+          <h5>We are waiting you, join us now!</h5>
         </div>
         <a href="https://forms.gle/LFfg5yYvkbNpr1Kq8" target="_blank">
           <div className="mb-6 inline-block w-auto rounded-full bg-blue-800">
             <div className="mb-2 ml-7 mr-5 mt-2 flex">
-              Join Now
+              <h5>Join Now</h5>
               <div className="ml-5 mt-1">
                 <IoIosArrowForward />
               </div>
