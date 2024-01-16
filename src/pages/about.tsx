d6f7da068694ea95387333f29fb5747754588cb0
whyiwant <youyuanchong@gmail.com>
2024-01-16 15:07:20 +0800

    Request: change about page font size to text-xl

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -17,7 +17,7 @@ const About = () => {
         <div className="mt-9 flex flex-col items-center bg-[#1d2129] text-white">
           <div className="mb-9 grid w-[90%] grid-cols-2 gap-x-2 gap-y-12">
             <div className="flex-co flex w-4/5 items-center">
-              <div>
+              <div className="text-xl">
                 FGA KL launched in the home of Dr & Mrs Koh Eng Kiat in Section
                 17; about 40 people. The Chinese Church, Youth Church, Finance
                 Department, Ladies&apos; Fellowship and School of Christian
