ab00720ab273c37bc23854227d85260add3a3bfd
Innis <1575270674@qq.com>
2024-07-09 01:31:29 +0800

    feat: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,36 @@
 import React from 'react'
+import Link from "next/link";
+import {useRouter} from "next/router";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    const navItems = [
+        {name: "Blog", link: "/blog"},
+        {name: "Products", link: "/products"},
+        {name: "Team", link: "/team"},
+        {name: "About", link: "/about"}
+    ]
+    const router = useRouter()
+
+
+    return (
+        <div className={"bg-black text-white flex items-center py-4 px-12 justify-between"}>
+            <div className={"flex items-center"}>
+                <div className={"text-4xl mr-12 cursor-pointer"}
+                        onClick={() => router.push("/")}
+                >FGA TECH</div>
+                <div className={"flex w-[300px] justify-between text-lg"}>
+                    {
+                        navItems.map((navItem) => {
+                            return <Link href={navItem.link} key={navItem.name} className={"cursor-pointer"}>
+                                {navItem.name}
+                            </Link>
+                        })
+                    }
+                </div>
+            </div>
+            <img src="/FGA_Tech_Logo.png" alt="FGA_Tech_Logo" className={"w-14 h-14 float-right"}/>
+        </div>
+    )
 }
 
 export default Header
