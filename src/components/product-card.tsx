d037dcac5f95f6c54177fbaeeb84d61cf3859720
Innis <1575270674@qq.com>
2024-07-10 09:19:56 +0800

    fix: Products title

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -20,7 +20,7 @@ const ProductCard: React.FC<ProductCardProps> = ({src, alt, title, span, url}) =
             />
             <div className='text-left p-4 '>
                 <a href={url} target='_blank' className='cursor-pointer'>
-                    <h4 className="mb-1 font-bold h-[30px] cursor-pointer">{title}</h4>
+                    <h4 className="mb-1 font-bold h-[30px] cursor-pointer text-white">{title}</h4>
                     {/*<div className='truncate w-full cursor-pointer text-white text-xl mt-2 mb-3'>{title}</div>*/}
                 </a>
                 <span className='flex text-[#6e6e73] w-full '>{span}</span>
