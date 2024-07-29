12fedbd30bbedb3adc76724a004a45670d4f14bc
Innis <1575270674@qq.com>
2024-07-29 23:27:57 +0800

    fix: product card style

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -24,7 +24,7 @@ const ProductCard: React.FC<ProductCardProps> = ({src, alt, title, description: 
             <div className='text-left p-4 '>
                 <div className={"flex justify-between"}>
                     <a href={url} target='_blank' className='cursor-pointer'>
-                        <h4 className="mb-1 font-bold h-[30px] cursor-pointer text-white text-[20px]">{title}</h4>
+                        <h4 className="mb-1 font-bold min-h-[30px] cursor-pointer text-white sm:text-[20px] text-[16px]">{title}</h4>
                         {/*<div className='truncate w-full cursor-pointer text-white text-xl mt-2 mb-3'>{title}</div>*/}
                     </a>
                     <div className={"flex"}>
@@ -41,7 +41,7 @@ const ProductCard: React.FC<ProductCardProps> = ({src, alt, title, description: 
                         }
                     </div>
                 </div>
-                <span className='flex text-white w-full '>{description}</span>
+                <span className='flex text-white w-full sm:text-base text-sm'>{description}</span>
                 <span className='text-[#6e6e73] w-full text-sm'>Launch at {launch_at}</span>
             </div>
             <div>
